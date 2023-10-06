41a2310a3d7e8b674fa7c058d50e3288c67a4a11
Hamid <www.abdulhamid3311@gmail.com>
2023-10-06 12:58:40 +0600

    Product details page done

---
+++ src/components/UI/ReviewDetails.js
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+
+
+const ReviewDetails = ({ Reviews }) => {
+    const [comment, setComment] = useState('')
+
+
+
+
+    //Handle Comment Form
+    const handleCommentForm = (e) => {
+        e.preventDefault();
+        console.log(comment)
+
+        /*  if (!user.email) {
+             toast.info("Please login For comment Here!");
+             return
+         }
+ 
+         const data = {
+             name: user.email,
+             comment: comment
+         }
+ 
+         postComment({ bookId, data })
+             .unwrap()
+             .then((response) => {
+                 console.log('Comment added successfully', response);
+                 toast.success("Comment Added Successfully!");
+                 setComment('')
+             })
+             .catch((error) => {
+                 console.error('Error adding Comment', error);
+                 toast.error("Comment Added Failed!")
+             }); */
+    };
+
+
+    return (
+        <div>
+            <form onSubmit={handleCommentForm}>
+                {/* <h1 className='font-bold'>Leave a comment</h1> */}
+                <div className="w-full" id="textarea">
+                    <textarea
+                        placeholder="Leave a comment..."
+                        onChange={(e) => setComment(e.target.value)}
+                        required rows={2}
+                        className='w-full' />
+                </div>
+                <button type='submit' className='bg-primary text-white px-3 py-1 rounded-[5px]  font-semibold '>Submit</button>
+            </form>
+
+            <div className='mt-8'>
+                <h3 className='text-base lg:text-lg font-bold  mb-2' >Comments:</h3>
+                {
+                    Reviews?.length !== 0 && Reviews?.map((rev, index) => <div key={index} className='flex items-center gap-2 '>
+                        <FaUserCircle className="text-2xl text-primary" />
+                        <div>
+                            <p className="font-bold " >abc@user</p>
+                            <p className='text-sm lg:text-base '>{rev.comment}</p>
+                        </div>
+                    </div>)
+                }
+
+                {Reviews?.length === 0 && null}
+            </div>
+        </div>
+
+    )
+};
+
+
+export default ReviewDetails;
--- src/pages/product-details/[id].js
+++ src/pages/product-details/[id].js
@@ -1,18 +1,66 @@
+import ReviewDetails from '@/components/UI/ReviewDetails';
 import RootLayout from '@/components/layouts/RootLayout';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-const ProductDetails = ({data}) => {
-    const router = useRouter()
-    const { id } = router.query;
+const ProductDetails = ({ data }) => {
+    console.log(data)
+    const { id, img, name, category, status, averageRating, price, description, individualRating, features, Reviews } = data;
 
-
-    console.log(id,data);
 
 
     return (
-        <div>
-            Product Details:{id}
+        <div className='bg-lightBg text-secondary '>
+            <div className='max-w-7xl mx-auto px-5 lg:px-0  py-10'>
+                <div className='flex flex-col lg:flex-row items-start gap-5 lg:gap-10 mb-5 lg:mb-10'>
+                    <div className='w-full lg:w-1/2 h-full lg:h-[400px]'>
+                        <Image src={img} alt={name} width={200} height={300} className='h-full w-full' />
+                    </div>
+
+                    <div className='w-full lg:w-1/2'>
+                        <div>
+                            <h1 className='text-primary text-xl lg:text-2xl font-bold mb-2'>{name}</h1>
+
+                            <div className='flex flex-wrap items-center gap-1 mb-2 font-bold text-sm lg:text-base'>
+                                <span className='bg-red-200 px-2 py-1 rounded-full font-semibold'>Category: {category}</span>
+                                <span className='bg-red-200 px-2 py-1 rounded-full font-semibold'>Status: {status}</span>
+                                <span className='bg-red-200 px-2 py-1 rounded-full font-semibold'> Rating: {individualRating}</span>
+                                <span className='bg-red-200 px-2 py-1 rounded-full font-semibold'>Avg. Rating: {averageRating}</span>
+                            </div>
+
+                            <p className=' font-bold mb-2'>Price: <span className='text-xl text-primary'>{price}</span> tk</p>
+
+                            <div>
+                                <p><span className=' font-bold text-sm lg:text-base'>description: </span>{description}</p>
+                            </div>
+
+                            <div>
+                                <h2 className=' font-bold mt-2'>Features:</h2>
+                                <ul className=' text-sm lg:text-base'>
+                                    {features.map((feat, index) => (
+                                        <li key={index}>
+                                            {feat.brand && <p>Brand: {feat.brand}</p>}
+                                            {feat.model && <p>Model: {feat.model}</p>}
+                                            {feat.specification && <p>Specification: {feat.specification}</p>}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+
+                <div className='w-full lg:w-1/2'>
+                    <h2 className=' font-bold mt-2 text-lg lg:text-2xl text-primary mb-5'>Reviews </h2>
+                    <ReviewDetails Reviews={Reviews} />
+                </div>
+            </div>
         </div>
     );
 };
@@ -20,18 +68,33 @@ const ProductDetails = ({data}) => {
 export default ProductDetails;
 
 
+
+
 ProductDetails.getLayout = function getLayout(page) {
     return <RootLayout>{page}</RootLayout>;
 };
 
 
-export const getStaticProps = async () => {
-    const res = await fetch(`http://localhost:5000/products/${id}`);
+export const getStaticPaths = async () => {
+    const res = await fetch(`http://localhost:5000/products`);
     const alldata = await res.json();
+
+    const paths = alldata.map(data => ({
+        params: { id: data.id.toString() }
+    }));
+
+    return { paths, fallback: false }
+}
+
+
+
+export const getStaticProps = async ({ params }) => {
+    const res = await fetch(`http://localhost:5000/products/${params.id}`);
+    const sinData = await res.json();
 
     return {
         props: {
-            data: alldata,
+            data: sinData,
         }
     }
 };
