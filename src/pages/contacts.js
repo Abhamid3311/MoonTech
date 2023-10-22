aac7a0b5e0d5b86d2b651c6b194068327240107a
Hamid <www.abdulhamid3311@gmail.com>
2023-10-22 16:03:06 +0600

    contacts page done

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -11,7 +11,7 @@ const Contacts = () => {
                     <h3 className='text-sm lg:text-base capitalize' >Call Us for any kind of query!</h3>
                 </div>
 
-                <div className='flex items-center gap-10 w-full'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10 w-full'>
 
 
                     <div className='flex items-center gap-5 bg-white rounded-md  w-full p-4'>
@@ -44,6 +44,61 @@ const Contacts = () => {
 
 
                 </div>
+
+                <div className='flex flex-col lg:flex-row items-start gap-8 my-8 w-full text-sm lg:text-base'>
+                    <div className='bg-white rounded-md w-full h-full  lg:h-[470px]'>
+                        <h1 className='bg-primary text-white rounded-t-md font-bold text-2xl px-4 py-2'>Customer Service</h1>
+
+                        <div className='p-4'>
+                            <h3 className='text-lg font-bold'>ORIGIN Lifetime Online Support</h3>
+                            <li>Support@originpc.com</li>
+
+
+                            <h3 className='text-lg font-bold mt-3'>24/7 Telephone Support</h3>
+                            <li> 1-877-ORIGIN-0 (674-4460) (US & Canada)</li>
+                            <li>305-971-1000 (International)</li>
+                            <li>1-888-852-0441 (Government & Corporate)</li>
+                            <li>CustomerService@originpc.com</li>
+
+                            <h3 className='text-lg font-bold mt-3'>Preexisting ORIGIN PC AU Customers</h3>
+                            <li>1300 904 021 (Australia)</li>
+                            <li>Support@originpc.com.au</li>
+                        </div>
+                    </div>
+
+
+                    <div className='bg-white rounded-md w-full  h-full  lg:h-[470px]'>
+                        <h1 className='bg-primary text-white rounded-t-md  font-bold text-2xl px-4 py-2'>Sales & Other</h1>
+
+                        <div className='p-4'>
+                            <h3 className='text-lg font-bold'>Sales Hours</h3>
+
+                            <h1>Monday - Friday</h1>
+                            <p>9:00am - 9:00pm EST</p>
+
+                            <h1 className='mt-2'> Saturday - Sunday, Holidays</h1>
+                            <p>9:00am - 6:00pm EST</p>
+
+
+                            <h3 className='text-lg font-bold mt-3'>Sales Support</h3>
+                            <li>Sales@originpc.com</li>
+
+                            <h3 className='text-lg font-bold mt-3'>Marketing</h3>
+                            <li> For marketing inquiries, send proposals to: Marketing@originpc.com</li>
+
+                            <h3 className='text-lg font-bold mt-3'>Government and Corporate</h3>
+                            <li> GCBD@originpc.com</li>
+
+                            <h3 className='text-lg font-bold mt-3'>General Inquries</h3>
+                            <li> Info@originpc.com</li>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+
 
 
 
