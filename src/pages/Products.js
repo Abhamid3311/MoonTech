cecf37bd3dc6fef836b167362af5fc6bb34cb8c6
Hamid <www.abdulhamid3311@gmail.com>
2023-10-23 14:51:33 +0600

    added category and status sorting

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -6,13 +6,16 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import { Checkbox, Label } from 'flowbite-react';
 import BackToTopButton from '@/components/utils/BottomToTop';
 
+const uniqueCategories = ["RAM", "Monitor", "Motherboard", "Processor", "Power Supply Unit", "Storage Device", "keyboard", "mouse", "headphones", "Accessories"]
+
 const Products = ({ allproducts }) => {
     const [searchQuery, setSearchQuery] = useState('');
     const [searchedProducts, setSearchedDeals] = useState([]);
-    const [selectedProduct, setSelectedCity] = useState([]);
     const inputRef = useRef(null);
-    const [searched, setSearched] = useState(false);
     const [priceSliderValue, setPriceSliderValue] = useState(null);
+
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [selectedStatus, setSelectedStatus] = useState([]);
 
 
     //Serached Filter Functionality
@@ -32,29 +35,26 @@ const Products = ({ allproducts }) => {
     }, [allproducts, searchQuery]);
 
 
-    /*  //checkBox Of City Name & Type  Functionality
-     useEffect(() => {
-         const filteredDeals = allproducts?.filter((book) => {
-             const lowerCaseQuery = searchQuery?.toLowerCase();
-             const hasMatchingCity = selectedProduct.length === 0 ||
-                 selectedProduct.includes(book?.name) ||
-                 selectedProduct.includes(book?.category) ||
-                 selectedProduct.includes(book?.price) ||
-                 selectedProduct.includes(book?.status);
- 
-             const isWithinPriceRange = parseInt(book.price) <= priceSliderValue;
-             return (
-                 hasMatchingCity && isWithinPriceRange &&
-                 (book.category.toLowerCase().includes(lowerCaseQuery) ||
-                     book.name.toLowerCase().includes(lowerCaseQuery) ||
-                     book.status.toLowerCase().includes(lowerCaseQuery) ||
-                     book.price.includes(lowerCaseQuery)
-                 )
-             )
-         });
- 
-         setSearchedDeals(filteredDeals);
-     }, [allproducts, searchQuery, selectedProduct, priceSliderValue]); */
+
+
+    useEffect(() => {
+        const filteredDeals = allproducts?.filter((product) => {
+            const lowerCaseQuery = searchQuery?.toLowerCase();
+            return (
+                (selectedCategories.length === 0 || selectedCategories.includes(product.category)) &&
+                (selectedStatus.length === 0 || selectedStatus.includes(product.status)) &&
+                (product.category.toLowerCase().includes(lowerCaseQuery) ||
+                    product.name.toLowerCase().includes(lowerCaseQuery) ||
+                    product.status.toLowerCase().includes(lowerCaseQuery) ||
+                    product.price.toLowerCase().includes(lowerCaseQuery))
+            );
+        });
+        setSearchedDeals(filteredDeals);
+    }, [allproducts, searchQuery, selectedCategories, selectedStatus]);
+
+
+
+
 
 
 
@@ -67,28 +67,30 @@ const Products = ({ allproducts }) => {
         }
     };
 
-
-    //Handle CheckBox Filter
-    const handleCheckboxChange = (e) => {
-        const { value } = e.currentTarget;
-        // console.log(value);
-
-        setselectedProduct((prevselectedCity) => {
-            if (prevselectedCity.includes(value)) {
-                return prevselectedCity.filter((genre) => genre !== value);
-            } else {
-                return [...prevselectedCity, value];
-            }
-        });
-    };
-
-
-
     //Handle Price Filter Slider
     const handlePriceSliderChange = (value) => {
         setPriceSliderValue(parseInt(value));
     };
 
+
+
+    ///Filter
+
+    const toggleCategory = (category) => {
+        if (selectedCategories.includes(category)) {
+            setSelectedCategories(selectedCategories.filter(c => c !== category));
+        } else {
+            setSelectedCategories([...selectedCategories, category]);
+        }
+    };
+
+    const toggleStatus = (status) => {
+        if (selectedStatus.includes(status)) {
+            setSelectedStatus(selectedStatus.filter(s => s !== status));
+        } else {
+            setSelectedStatus([...selectedStatus, status]);
+        }
+    };
 
 
 
@@ -136,79 +138,48 @@ const Products = ({ allproducts }) => {
                         {/* CheckBox Sorting */}
                         <div className='flex flex-row lg:flex-col gap-5 items-start'>
 
-                            {/* CheckBox Sorting By City Name */}
+                            {/* CheckBox Sorting By Availability */}
 
                             <div className="ml-2">
-                                <h2 className="text-lg  text-start">Availability</h2>
-
+                                <h2 className="text-lg text-start">Availability</h2>
                                 <div className="flex items-center gap-2">
                                     <Checkbox
-                                        id="Stock"
-                                        value="RAM"
-                                        checked={selectedProduct.includes("RAM")}
-                                        onChange={handleCheckboxChange}
+                                        id="InStock"
+                                        checked={selectedStatus.includes("In Stock")}
+                                        onChange={() => toggleStatus("In Stock")}
+                                        value="In Stock"
                                     />
-                                    <Label htmlFor="Stock">RAM</Label>
+                                    <Label htmlFor="InStock">In Stock</Label>
                                 </div>
-
                                 <div className="flex items-center gap-2">
                                     <Checkbox
                                         id="OutofStock"
+                                        checked={selectedStatus.includes("Out of Stock")}
+                                        onChange={() => toggleStatus("Out of Stock")}
                                         value="Out of Stock"
-                                        checked={selectedProduct.includes("Out of Stock")}
-                                        onChange={handleCheckboxChange}
                                     />
                                     <Label htmlFor="OutofStock">Out of Stock</Label>
                                 </div>
-
-
-                                <hr className='bg-primary my-2' />
-
-
                             </div>
 
 
-                            {/*  Check Box Class Type */}
-                            {/*  <div>
-                                <h2 className="text-lg  text-start">Class Type</h2>
-                                <div className="ml-2">
-                                    <div className="flex items-center gap-2">
-                                        <Checkbox
-                                            id="Economy"
-                                            checked={selectedProduct.includes("Economy")}
-                                            onChange={handleCheckboxChange}
-                                            value={"Economy"} />
-                                        <Label htmlFor="Economy">Economy</Label>
-                                    </div>
 
-                                    <div className="flex items-center gap-2">
-                                        <Checkbox
-                                            id="Business"
-                                            checked={selectedProduct.includes("Business")}
-                                            onChange={handleCheckboxChange}
-                                            value={"Business"} />
-                                        <Label htmlFor="Business">Business</Label>
-                                    </div>
+                            {/* CheckBox Sorting By Category */}
 
-                                    <div className="flex items-center gap-2">
+                            <div className="ml-2">
+                                <h2 className="text-lg text-start">Category</h2>
+                                {uniqueCategories.map(category => (
+                                    <div key={category} className="flex items-center gap-2">
                                         <Checkbox
-                                            id="Premium-Economy"
-                                            checked={selectedProduct.includes("Premium Economy")}
-                                            onChange={handleCheckboxChange}
-                                            value={"Premium Economy"} />
-                                        <Label htmlFor="Premium-Economy">Premium Economy</Label>
+                                            id={category}
+                                            checked={selectedCategories.includes(category)}
+                                            onChange={() => toggleCategory(category)}
+                                            value={category}
+                                        />
+                                        <Label htmlFor={category}>{category}</Label>
                                     </div>
-
-                                    <div className="flex items-center gap-2">
-                                        <Checkbox
-                                            id="First-Class"
-                                            checked={selectedProduct.includes("First Class")}
-                                            onChange={handleCheckboxChange}
-                                            value={"First Class"} />
-                                        <Label htmlFor="First-Class">First Class</Label>
-                                    </div>
-                                </div>
-                            </div> */}
+                                ))}
+                            </div>
 
                         </div>
 
@@ -254,7 +225,7 @@ const Products = ({ allproducts }) => {
 
 
 
-           
+
 
         </div>
     );
